4b4b6a8b5ed90d4ef4aa63df1e56f938e4d1336a
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-20 22:33:19 +0200

    c11 commit

--- src/test/java/com/cursor/TeacherRestControllerTest.java
+++ src/test/java/com/cursor/TeacherRestControllerTest.java
@@ -1,15 +1,18 @@
 package com.cursor;
 
 import com.cursor.entity.Student;
+import com.cursor.entity.StudentsGroup;
 import com.cursor.entity.Teacher;
 import com.cursor.service.serviceInterfaces.StudentTableService;
 import com.cursor.service.serviceInterfaces.TeacherTableService;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -57,5 +61,36 @@ public class TeacherRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Will"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Peterson"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(44));
+    }
+
+    @Test
+    public void testAddGroupForTeacher() throws Exception
+    {
+        Gson gson = new Gson();
+        String requestBody = gson.toJson(new StudentsGroup("firstGroup"));
+
+        this.mockMvc.perform(
+                        post("/teacher/addGroupForTeacher").param("groupId", "1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddTeacher() throws Exception
+    {
+        Gson gson = new Gson();
+        String requestBody = gson.toJson(new Teacher("Will","Peterson","male",44));
+
+        this.mockMvc.perform(post("/teacher/addTeacher")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testTeacherNumberOfGroups() throws Exception
+    {
+
     }
 }
