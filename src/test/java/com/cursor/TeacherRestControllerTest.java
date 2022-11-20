0cecfc8a5d0a3682bdf3145f2d2013bad6f1294f
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-20 22:07:41 +0200

    c9 commit

--- src/test/java/com/cursor/TeacherRestControllerTest.java
+++ src/test/java/com/cursor/TeacherRestControllerTest.java
@@ -1,5 +1,7 @@
 package com.cursor;
 
+import com.cursor.entity.Student;
+import com.cursor.entity.Teacher;
 import com.cursor.service.serviceInterfaces.StudentTableService;
 import com.cursor.service.serviceInterfaces.TeacherTableService;
 import org.junit.Test;
@@ -10,6 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -25,6 +35,16 @@ public class TeacherRestControllerTest {
     @Test
     public void testGetAll() throws Exception
     {
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(new Teacher("Will","Peterson","male",44));
 
+        when(teacherTableServiceMock.getAllTeacher()).thenReturn(teacherList);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/teacher/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Will"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("Peterson"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(44));
     }
 }
