d52ee57c67fc2501efb52e2edd2630eb86077cf3
Sergey <xsergey87x@ukr.net>
2022-11-22 00:11:23 +0200

    e23 commit

--- src/test/java/com/cursor/serviceTest/TeacherTableServiceImplTest.java
+++ src/test/java/com/cursor/serviceTest/TeacherTableServiceImplTest.java
@@ -1,9 +1,28 @@
 package com.cursor.serviceTest;
 
+import com.cursor.entity.Teacher;
+import com.cursor.repository.StudentRepository;
+import com.cursor.repository.StudentsGroupRepository;
+import com.cursor.repository.TeacherRepository;
+import com.cursor.service.GroupTableServiceImpl;
+import com.cursor.service.TeacherTableServiceImpl;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class TeacherTableServiceImplTest {
+
+    @Mock
+    private TeacherRepository teacherRepositoryMock = Mockito.mock(TeacherRepository.class);;
+
+    @Mock
+    private StudentsGroupRepository studentsGroupRepositoryMock = Mockito.mock(StudentsGroupRepository.class);;
+
+    @InjectMocks
+    private TeacherTableServiceImpl teacherTableServiceMock = new TeacherTableServiceImpl(teacherRepositoryMock,studentsGroupRepositoryMock);
+
 
 }
